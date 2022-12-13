431eb55512069a529d86bf5ce18460754715b05c
MisaelCordova <misael.cordova.f@gmail.com>
2022-12-13 17:11:31 -0300

    codigo 13_12

---
+++ 50-OitavoProjeto/BaseDeDados.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _50_OitavoProjeto
+{
+    internal class BaseDeDados
+    {
+        private List<CadastroPessoa> ListaDePessoas;
+
+        public void AdicionaPessoa(CadastroPessoa pessoa)
+        {
+            ListaDePessoas.Add(pessoa);
+        }
+        public List<CadastroPessoa> PesquisaPessoaPorDoc(string numeroDeDocumento)
+        {
+            List<CadastroPessoa> ListaDePessoasTemp = ListaDePessoas.Where(x=>x.NumeroDoDocumento== numeroDeDocumento).ToList();
+
+            if(ListaDePessoasTemp.Count>0)
+                return ListaDePessoasTemp;
+            else
+            {
+                return null;
+            }
+        }
+        public  List<CadastroPessoa> RemoverPessoaPorDoc(string numeroDeDocumento) 
+        {
+            List<CadastroPessoa> ListaDePessoasTemp = ListaDePessoas.Where(x=>x.NumeroDoDocumento == numeroDeDocumento).ToList();
+            if (ListaDePessoasTemp.Count > 0) 
+            {
+                foreach (CadastroPessoa pessoa in ListaDePessoasTemp)
+                {
+                    ListaDePessoas.Remove(pessoa);
+                }
+                return ListaDePessoasTemp;
+            }
+                
+            else
+            {
+            
+                return null;
+            }
+        }
+        public BaseDeDados()
+        {
+            ListaDePessoas = new List<CadastroPessoa>();
+        }
+    }
+}
---
+++ 50-OitavoProjeto/CadastroPessoa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _50_OitavoProjeto
+{
+    internal class CadastroPessoa
+    {
+        private string nome;
+        private string numeroDoDocumento;
+        private DateTime dataNasci;
+        private string nomeDarua;
+        private UInt32 numeroDaCasa;
+
+        public CadastroPessoa(string nome, string numeroDoDocumento, DateTime dataNasci, string nomeDarua, uint numeroDaCasa)
+        {
+            this.nome = nome;
+            this.numeroDoDocumento = numeroDoDocumento;
+            this.dataNasci = dataNasci;
+            this.nomeDarua = nomeDarua;
+            this.numeroDaCasa = numeroDaCasa;
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public string NumeroDoDocumento { get => numeroDoDocumento; set => numeroDoDocumento = value; }
+        public DateTime DataNasci { get => dataNasci; set => dataNasci = value; }
+        public string NomeDarua { get => nomeDarua; set => nomeDarua = value; }
+        public uint NumeroDaCasa { get => numeroDaCasa; set => numeroDaCasa = value; }
+    }
+}

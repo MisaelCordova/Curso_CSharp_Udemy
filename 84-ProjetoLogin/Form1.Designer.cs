2bbf1f9d568fdd2684485bb7613979b86da87052
MisaelCordova <misael.cordova.f@gmail.com>
2022-12-19 14:31:38 -0300

    codigo 19_12-1

---
+++ 84-ProjetoLogin/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace _84_ProjetoLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbllogin = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.mskSenha = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // lbllogin
+            // 
+            this.lbllogin.AutoSize = true;
+            this.lbllogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbllogin.Location = new System.Drawing.Point(72, 27);
+            this.lbllogin.Name = "lbllogin";
+            this.lbllogin.Size = new System.Drawing.Size(190, 73);
+            this.lbllogin.TabIndex = 0;
+            this.lbllogin.Text = "Login";
+            this.lbllogin.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(104, 143);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(206, 26);
+            this.txtUsuario.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Usuário:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Senha :";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(23, 226);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(287, 46);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // mskSenha
+            // 
+            this.mskSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mskSenha.Location = new System.Drawing.Point(104, 181);
+            this.mskSenha.Name = "mskSenha";
+            this.mskSenha.PasswordChar = '*';
+            this.mskSenha.Size = new System.Drawing.Size(206, 26);
+            this.mskSenha.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 284);
+            this.Controls.Add(this.mskSenha);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lbllogin);
+            this.MaximumSize = new System.Drawing.Size(357, 323);
+            this.Name = "Form1";
+            this.Text = "Usuário :";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbllogin;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.MaskedTextBox mskSenha;
+    }
+}
+
---
+++ 84-ProjetoLogin/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _84_ProjetoLogin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(btnLogin.Text == "Login")
+            {
+                if (txtUsuario.Text == "")
+                    MessageBox.Show("Digite o nome do usuário", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                {
+                    if (txtUsuario.Text == "Misael")
+                    {
+                        if (mskSenha.Text == "")
+                            MessageBox.Show("Digite a senha!!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        else
+                        {
+                            if (mskSenha.Text == "1234")
+                            {
+                                MessageBox.Show("Usuário Logado com sucesso", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                lbllogin.Text = "Logado!";
+                                lbllogin.ForeColor = Color.Green;
+                                txtUsuario.Text = "";
+                                mskSenha.Text = "";
+                                btnLogin.Text = "Logout";
+                            }
+                            else
+                            {
+                                MessageBox.Show("Senha Incorreta!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                    else
+                        MessageBox.Show("Usuário incorreto!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            } else
+            {
+                lbllogin.Text = "Logado!";
+                lbllogin.ForeColor = Color.Black;
+                txtUsuario.Text = "";
+                mskSenha.Text = "";
+                btnLogin.Text = "Login";
+            }
+          
+        } 
+    }
+}
